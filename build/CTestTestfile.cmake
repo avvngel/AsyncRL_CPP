0192399de28809c1a0abbe6b9cc65d2f751b27be
avvngel <angelanselmoleal@gmail.com>
2024-08-07 14:44:37 -0500

    removed build dir from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/angelleal/Documents/Projects/AsyncRL_CPP
-# Build directory: /home/angelleal/Documents/Projects/AsyncRL_CPP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- build/tests/CMakeFiles/test_funct_approx.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_funct_approx.
-# This may be replaced when dependencies are built.
