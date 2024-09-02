57a4d53da242d5edeafe5e88c4a304de46fb0ea7
avvngel <angelanselmoleal@gmail.com>
2024-09-02 14:27:54 -0500

    revised concepts for indentifying discrete vs continuous action spaces

--- include/concepts.h
+++ include/concepts.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <type_traits>
+#include "torch/torch.h"
+#include "action_space.h"
 
 template <typename T>
-concept DiscreteAction = std::is_integral_v<T>;
+concept DiscreteAS = std::is_same_v<T, DiscreteUniformActionSpace> 
+                  || std::is_same_v<T, DiscreteVariableActionSpace>;
 
 template <typename T>
-concept ContinuousAction = std::is_floating_point_v<T>;
+concept ContinuousAS = std::is_same_v<T, ContinuousUniformActionSpace>
+                    || std::is_same_v<T, ContinuousVariableActionSpace>;
+
+template <typename T>
+concept IsActionSpace = DiscreteAS<T> || ContinuousAS<T>;
 
 template <typename T>
 concept HasSize = requires( T t )
